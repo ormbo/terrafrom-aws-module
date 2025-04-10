f86c32102a6f45d081768c69f0bbe7c750481cc8
ormbo <or.bohadana@midlink.co.il>
2025-04-10 10:29:40 +0300

    Update main.tf

---
+++ tf-module-ec2-sgw/example/main.tf
@@ -0,0 +1,15 @@
+ module "ec2_sgw" {
+   source     = "../../../terrafrom-aws-module/tf-module-ec2-sgw"
+   name                               = "ec2-storage-gateway" 
+   availability_zone                  = data.aws_availability_zones.available.names[0] 
+   subnet_id                          = "subnet-0f223283350d92e03"
+   ingress_cidr_block_activation      = "10.0.1.0/28"
+   storagegateway_name                = "Test-Storage-Gateway"
+   ssh_key_name                       = "storage-gateway"
+   gateway_type                       = "FILE_S3"
+   create_vpc_endpoint                = true
+   create_security_group              = true
+   create_vpc_endpoint_sg             = true 
+   vpc_id                             = "vpc-06e7dd7eccec7a493"
+   timezone                           = "GMT+2:00"
+ }
