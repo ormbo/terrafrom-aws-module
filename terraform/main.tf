51962eb3ae99b00a3d9bfefd43e5f3a1898b7dbe
Or Moshe Bohadana <or.bohadana@midlink.co.il>
2025-06-04 13:38:18 +0300

    Update Workflows

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,42 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.18.1"
+  name            = var.vpc_name
+  cidr            = var.cidr
+  azs             = var.azs-vpc
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = var.tags
+}
+
+module "endpoints" {
+    source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+    version = "5.19.0"
+
+    vpc_id                      =   module.vpc.vpc_id
+    create_security_group       =   true
+    security_group_name         =   "SandBox-Endpoint-SG"
+    endpoints                   =   {
+        endpoint-secretsmanager =   {
+            service             =   "secretsmanager"
+            private_dns_enabled =   true
+            subnet_ids          =   module.vpc.private_subnets
+            },  
+        endpoint-s3             =   {
+            service             =   "s3"
+            service_type        =   "Gateway"
+            route_table_ids     =   module.vpc.private_route_table_ids
+            },
+    }
+    tags                        =   var.tags
+    depends_on                  =   [module.vpc]
+}
+
 
 resource "aws_security_group" "security_group_ec2" {
     name        = "SandBox-SG"
